b2472b9796f81c16c5971b9b0f759f333eb54d0b
GosGuru <mporcile59@gmail.com>
2025-01-26 21:54:27 -0300

    Update FooterMovil.js

--- app/components/FooterMovil.js
+++ app/components/FooterMovil.js
@@ -16,19 +16,19 @@ const FooterMovil = () => {
     <div className="footer">
       <button className="footer-button" onClick={handleBack}>
         <Home className="footer-icon" />
-        <span>Volver</span>
+        <span>Volver</span> {/* Icono de Home */}
       </button>
       <button className="footer-button">
         <Search className="footer-icon" />
-        <span>Buscar</span>
+        <span>Buscar</span> {/* Icono de búsqueda */}
       </button>
       <button className="footer-button">
         <AccountCircle className="footer-icon" />
-        <span>Perfil</span>
+        <span>Perfil</span> {/* Icono de perfil */}
       </button>
       <button className="footer-button">
         <Settings className="footer-icon" />
-        <span>Ajustes</span>
+        <span>Configuración</span> {/* Icono de configuración */}
       </button>
     </div>
   );
