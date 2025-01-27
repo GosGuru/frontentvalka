b87e5b6b03620d949f0cabe19a7d372a473882ef
GosGuru <mporcile59@gmail.com>
2025-01-27 18:28:56 -0300

    Update getEjerciciosVideo.js

--- app/api/getEjerciciosVideo.js
+++ app/api/getEjerciciosVideo.js
@@ -4,12 +4,15 @@ import { ENV } from "../utils";
 // src/api/getEjercicioConVideo.js
 export const getEjercicioConVideo = async (exerciseId) => {
   try {
-    
+        const token = localStorage.getItem("token");  // Obtener el token dinámicamente
+    if (!token) {
+      throw new Error("Falta el token de autenticación");
+    }
     const url = `${ENV.API_BASE_URL}/api/ejercicios/${exerciseId}?populate=videoURL`; // Asegura que se incluye 'videoURL'
     console.log(url);
     const response = await fetch(url, {
       headers: {
-        Authorization: `Bearer ${ENV.TOKEN}`,
+      Authorization: `Bearer ${token}`,
       },
     });
 
